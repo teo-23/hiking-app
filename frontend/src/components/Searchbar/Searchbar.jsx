31b5d4cb930b2a944a33ee4de7d8f6308c11f632
MichielSnoek <michiel.snoek@gmail.com>
2020-05-28 19:53:27 +0200

    started working on the search bar

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import './searchbar.css'
+import hikingBanner from './hiking-banner.jpeg'
 
 export default function SearchBar(props) {
     const [value, setValue] = useState('')
 
     return (
-        <div>
-            <form action="">
-                <input class="search-bar" type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-            </form>
+        <div class="search-container">
+         
+                <img id="hiking-banner" src={hikingBanner} alt=""/>
+                <form class="centered" action="">
+                    <input class="search-bar" type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+                </form>
+         
         </div>
-    )
+    )   
 }
+ 
